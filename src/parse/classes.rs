4447db41186021750d222bce677fb179d6914864
slice <tinyslices@gmail.com>
2021-05-13 17:36:54 -0700

    split parse module into multiple files

---
+++ src/parse/classes.rs
@@ -0,0 +1,89 @@
+use std::collections::HashMap;
+
+extern crate swc_ecma_ast as ast;
+use swc_ecma_visit::{Visit, VisitWith};
+
+use super::ParseError;
+
+pub type ClassMappingMap = HashMap<String, String>;
+
+// NOTE(slice): More like `ClassMappingModulesMap`, but that's too long.
+pub type ClassModuleMap = HashMap<u64 /* webpack module id */, ClassMappingMap>;
+
+struct ClassMappingVisitor {
+    classes: ClassMappingMap,
+}
+
+// NOTE(slice): It's worth noting that these visitors will stop visiting deeper
+// if you do not explicitly call `visit_children_with` within the visitor
+// methods.
+//
+// However, we don't need to do that because there's only 2 levels of
+// key-values. The outer level is handled by `ClassesModuleVisitor` and the
+// inner by `ClassesClassMappingVisitor`.
+
+impl Visit for ClassMappingVisitor {
+    fn visit_key_value_prop(&mut self, n: &ast::KeyValueProp, _parent: &dyn swc_ecma_visit::Node) {
+        let key: &str = match &n.key {
+            // { a: ... }
+            ast::PropName::Ident(ast::Ident { sym: atom, .. }) => &atom,
+            // { "some key": ... }
+            ast::PropName::Str(ast::Str { value: atom, .. }) => &atom,
+            _ => return,
+        };
+
+        let value: &str = match &*n.value {
+            ast::Expr::Lit(ast::Lit::Str(ast::Str { value: atom, .. })) => &atom,
+            _ => return,
+        };
+
+        self.classes.insert(key.to_string(), value.to_string());
+    }
+}
+
+struct ClassModuleVisitor {
+    modules: ClassModuleMap,
+}
+
+impl Visit for ClassModuleVisitor {
+    fn visit_key_value_prop(&mut self, n: &ast::KeyValueProp, _parent: &dyn swc_ecma_visit::Node) {
+        let module_id = match &n.key {
+            // wow, i sure do hope webpack doesn't start using floating-point
+            // numbers for module ids
+            ast::PropName::Num(ast::Number { value, .. }) => *value as u64,
+            _ => return,
+        };
+
+        let mut class_mapping_visitor = ClassMappingVisitor {
+            classes: HashMap::new(),
+        };
+        n.visit_children_with(&mut class_mapping_visitor);
+
+        self.modules
+            .insert(module_id, class_mapping_visitor.classes);
+    }
+}
+
+/// Walk a Webpack chunk script containing classname mappings.
+pub fn walk_classes_chunk(script: &ast::Script) -> Result<ClassModuleMap, ParseError> {
+    let mut visitor = ClassModuleVisitor {
+        modules: HashMap::new(),
+    };
+    crate::util::measure("visiting class modules and mapping", || {
+        script.visit_children_with(&mut visitor)
+    });
+
+    let total_mappings: usize = visitor
+        .modules
+        .iter()
+        .map(|(_, mappings)| mappings.len())
+        .sum();
+
+    log::debug!(
+        "visited {} class module(s), totalling to {} class mappings",
+        visitor.modules.len(),
+        total_mappings
+    );
+
+    Ok(visitor.modules)
+}
---
+++ src/parse/mod.rs
@@ -0,0 +1,42 @@
+pub mod classes;
+pub mod webpack;
+
+pub use classes::*;
+pub use webpack::*;
+
+use swc_common::{sync::Lrc, FileName, SourceMap};
+use swc_ecma_parser::{error::Error as SwcError, lexer::Lexer, Parser, StringInput, Syntax};
+extern crate swc_ecma_ast as ast;
+use thiserror::Error;
+
+/// Parses a script.
+pub fn parse_script(js: &str) -> Result<ast::Script, ParseError> {
+    let cm: Lrc<SourceMap> = Default::default();
+    let fm = cm.new_source_file(FileName::Custom("script.js".into()), js.into());
+
+    let lexer = Lexer::new(
+        Syntax::Es(Default::default()),
+        // JscTarget = es5
+        Default::default(),
+        StringInput::from(&*fm),
+        None,
+    );
+
+    let mut parser = Parser::new_from(lexer);
+    Ok(parser.parse_script()?)
+}
+
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("missing ast node: {0}")]
+    MissingNode(&'static str),
+
+    #[error("parsing error")]
+    Swc(SwcError),
+}
+
+impl From<SwcError> for ParseError {
+    fn from(err: SwcError) -> Self {
+        ParseError::Swc(err)
+    }
+}
