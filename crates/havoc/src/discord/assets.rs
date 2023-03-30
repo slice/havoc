eb511cd0341d2384fb69c81abc23224c1fdbedfa
slice <tinyslices@gmail.com>
2023-03-29 17:28:50 -0700

    rename assets::assets to assets::collections

--- crates/havoc/src/discord/assets.rs
+++ crates/havoc/src/discord/assets.rs
@@ -1,9 +1,9 @@
-mod assets;
 mod cache;
+mod collections;
 mod frontend;
 mod root;
 
-pub use assets::Assets;
 pub use cache::{AnyError, AssetCache, AssetContent, AssetPreprocessor};
+pub use collections::Assets;
 pub use frontend::{FeAsset, FeAssetType};
 pub use root::RootScript;
